3fc48bee45c058a00815584fd5d2f411b71e96af
wschern <schern@Brandons-iMac-2.local>
2020-02-28 16:14:18 +0100

    Update for Contao 4.9

--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -14,7 +14,23 @@
 /**
  * Table tl_page
  */
-$GLOBALS['TL_DCA']['tl_page']['palettes']['root'] = str_replace(';{layout_legend', ';{rsm_google_recaptcha_legend:hide},rsm_public_key,rsm_private_key;{layout_legend', $GLOBALS['TL_DCA']['tl_page']['palettes']['root']);
+// $GLOBALS['TL_DCA']['tl_page']['palettes']['root'] = str_replace(';{layout_legend', ';{rsm_google_recaptcha_legend:hide},rsm_public_key,rsm_private_key;{layout_legend', $GLOBALS['TL_DCA']['tl_page']['palettes']['root']);
+
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
+
+PaletteManipulator::create()
+    ->addLegend('rsm_google_recaptcha_legend', '')
+    ->addField('rsm_public_key', 'rsm_google_recaptcha_legend')
+    ->addField('rsm_private_key', 'rsm_google_recaptcha_legend')
+    ->applyToPalette('rootfallback', 'tl_page')
+;
+
+PaletteManipulator::create()
+    ->addLegend('rsm_google_recaptcha_legend', '')
+    ->addField('rsm_public_key', 'rsm_google_recaptcha_legend')
+    ->addField('rsm_private_key', 'rsm_google_recaptcha_legend')
+    ->applyToPalette('root', 'tl_page')
+;
 
 $GLOBALS['TL_DCA']['tl_page']['fields']['rsm_public_key'] = array(
     'label'         => &$GLOBALS['TL_LANG']['tl_page']['rsm_public_key'],
